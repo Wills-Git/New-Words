f3d8cd8bd8591b4d3b6136fc8cc2c7d41b720d1a
Wills-Git <williaminterlock@gmail.com>
2023-05-21 23:30:03 -0500

    changing user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,7 +16,7 @@ function App(){
 
     return(
         <div>
-            <h1>New Words</h1>
+            <h1>New Words v1</h1>
             <ul>{
             users.map(user => (
                 <li>{`Name: ${user.username}`}/br
