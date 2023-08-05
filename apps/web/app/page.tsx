ffdeecc7c01d0da26477105577730f06315fd388
user.email <williaminterlock@gmail.com>
2023-08-04 21:39:01 -0500

    init

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,33 @@
+import { trpc } from '@web/app/trpc';
+import{ useSession, signIn, signOut } from 'next-auth/react'
+import Link from 'next/link';
+
+export default function Home() {
+
+  async function greeting(){
+      const { greeting } = await trpc.hello.query({ name: 'yofel' });
+      return greeting
+  }
+  console.log(greeting);
+
+  const {data: session, status } = useSession()
+
+  if( status === 'loading') {
+    return <div>Loading</div>
+  }
+
+  return (
+    <div className='p-4'>
+      <h1 className='font-bold mb-6'> Home Page</h1>
+      <div className="mb-6 flex gap-3">
+        <Link href='api/auth/signin'>
+          <p className="bg-emerald-400 text-sm rounder px-3 py-1 text-white">sign in</p>
+        </Link>
+      </div>
+    </div>
+  );
+
+
+
+  return <div>{greeting()}</div>;
+}
